15f45d4f099ab4379749942b1309488eaee68aac
maltaweel <m.altaweel@ucl.ac.uk>
2016-10-29 11:21:58 +0100

    commit

--- src/scrape/facebookScraper.py
+++ src/scrape/facebookScraper.py
@@ -4,13 +4,24 @@ Created on Oct 28, 2016
 @author: maltaweel
 '''
 import facebook
+from facebook import requests
+
+def getSearch():
+        token = 'EAACEdEose0cBAJLrzut4iiCif3Ff3ZCGLGCIXuWYqTV5RC9ymAGc0GotTH71vsoLexaPXVHRzx9ZAnhZAiZBEgZCtGuBQrm3D0ryjFBupO24LNQlZBJ1B4BEEMgvOf1bZBjwyYMENjAZCXnapmKRK5J2OrPA4rINdfB64hZBeJlRVEgZDZD'
+
+        graph = facebook.GraphAPI(token)
+        profile = graph.get_object("me")
+        print profile
+
+        query="iron age"
+        p=requests.get("https://graph.facebook.com/search?access_token=" + token +  "&q=" + query + "&type=page")
+        list=[]
+        for i in p:
+            list.append(i)
+
+        return list
 
 
-token = 'EAACEdEose0cBAJLrzut4iiCif3Ff3ZCGLGCIXuWYqTV5RC9ymAGc0GotTH71vsoLexaPXVHRzx9ZAnhZAiZBEgZCtGuBQrm3D0ryjFBupO24LNQlZBJ1B4BEEMgvOf1bZBjwyYMENjAZCXnapmKRK5J2OrPA4rINdfB64hZBeJlRVEgZDZD'
-
-graph = facebook.GraphAPI(token)
-profile = graph.get_object("me")
-print profile
-
-        
+list=getSearch()
+print(list)    
 
